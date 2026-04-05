b4ce0e27a97c8ff8434bdcbbe13c0a52186d797b
iulian-lucaci <55348313+iulian-lucaci@users.noreply.github.com>
2026-04-05 10:07:10 +0000

    Image improvements: lightbox gallery

--- _plugins/responsive_image_srcset.rb
+++ _plugins/responsive_image_srcset.rb
@@ -1,36 +1,70 @@
-# Adds responsive srcset attributes to local markdown images when paired variants exist.
+# Adds responsive srcset, lazy loading, async decoding, and lightbox anchors to local markdown images.
 # It only processes markdown-generated pages/posts to avoid rewriting inline JS strings.
+
+require 'cgi'
 
 module Jekyll
   module ResponsiveImageSrcset
-    IMAGE_TAG_PATTERN = /<img\b([^>]*?)\bsrc=(['"])([^'">]+?)\2([^>]*?)>/i
+    IMAGE_TAG_PATTERN = /<img\b([^>]*?)>/i
 
     def self.process_html(html, page)
       return html unless html
 
-      html.gsub(IMAGE_TAG_PATTERN) do |img_tag|
-        next img_tag if img_tag =~ /\bsrcset=/i
+      html = add_lightbox_anchors(html, page)
 
+      html.gsub(IMAGE_TAG_PATTERN) do |img_tag|
         src_match = img_tag.match(/src=(['"])([^'">]+?)\1/i)
         src = src_match && src_match[2]
         next img_tag unless src
         next img_tag if src =~ %r{\A(?:https?:|data:)}i
 
+        img_tag = add_loading_lazy(img_tag)
+        img_tag = add_decoding_async(img_tag)
+
         src_path = src.sub(%r{\A/}, '')
         source_path = File.join(page.site.source, src_path)
         variant_path = find_variant(source_path)
-        next img_tag unless variant_path
 
-        srcset = build_srcset(src, source_path, variant_path)
-        next img_tag unless srcset
+        if variant_path
+          srcset = build_srcset(src, source_path, variant_path)
+          img_tag = img_tag.sub(/(\s*\/?>)\z/, " srcset=\"#{srcset}\" sizes=\"(max-width: 768px) 100vw, 768px\"\1") if srcset
+        end
 
-        img_tag = add_loading_lazy(img_tag)
-        img_tag.sub(/(\s*\/?>)\z/, " srcset=\"#{srcset}\" sizes=\"(max-width: 768px) 100vw, 768px\"\1")
+        img_tag
+      end
+    end
+
+    def self.add_lightbox_anchors(html, page)
+      gallery_id = "glightbox-#{page.url.gsub(/[^a-z0-9]+/i, '-') }"
+
+      html.gsub(IMAGE_TAG_PATTERN) do |img_tag|
+        position = Regexp.last_match.begin(0)
+        before = html[0...position]
+
+        if before.rindex('</a>') && before.rindex('<a') && before.rindex('<a') > before.rindex('</a>')
+          next img_tag
+        end
+
+        src_match = img_tag.match(/src=(['"])([^'">]+?)\1/i)
+        next img_tag unless src_match
+        src = src_match[2]
+        next img_tag if src =~ %r{\A(?:https?:|data:)}i
+
+        alt_match = img_tag.match(/alt=(['"])([^'">]*?)\1/i)
+        title_attr = alt_match ? escape_html(alt_match[2]) : nil
+
+        link_attrs = ["href=\"#{src}\"", 'class="glightbox"', "data-gallery=\"#{gallery_id}\""]
+        link_attrs << "data-title=\"#{title_attr}\"" if title_attr
+
+        "<a #{link_attrs.join(' ')}>#{img_tag}</a>"
       end
     end
 
     def self.add_loading_lazy(img_tag)
-      img_tag = add_attribute(img_tag, 'loading', 'lazy')
+      add_attribute(img_tag, 'loading', 'lazy')
+    end
+
+    def self.add_decoding_async(img_tag)
       add_attribute(img_tag, 'decoding', 'async')
     end
 
@@ -61,7 +95,6 @@ module Jekyll
       sizes = variant_sizes(source_path, variant_path)
       return nil unless sizes
 
-      variant_url = src.sub(/( - [12])(\.[^.]+)\z/, '\1\2')
       variant_url = if src.end_with?(" - 1#{File.extname(src)}")
                       src.sub(/ - 1(\.[^.]+)\z/, ' - 2\1')
                     elsif src.end_with?(" - 2#{File.extname(src)}")
@@ -88,6 +121,10 @@ module Jekyll
       else
         { small: variant_path, large: src_path }
       end
+    end
+
+    def self.escape_html(value)
+      CGI.escapeHTML(value.to_s)
     end
   end
 end
