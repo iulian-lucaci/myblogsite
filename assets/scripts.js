86e4ff7dba92d646b92624752aca1d07ecaedfd7
iulian-lucaci <55348313+iulian-lucaci@users.noreply.github.com>
2026-06-07 08:50:33 +0000

    Newsletter signup: Mailchimp/ConvertKit form + opt-in flow.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,16 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+
+  var $newsletterForm = $('#newsletterForm');
+  if ($newsletterForm.length) {
+    $newsletterForm.on('submit', function () {
+      var $button = $(this).find('button[type=submit]');
+      $button.prop('disabled', true).text('Submitting...');
+      $('#newsletterMessage').removeClass('alert-success alert-danger').text('');
+      setTimeout(function () {
+        $button.prop('disabled', false).text('Subscribe');
+        $('#newsletterMessage').addClass('alert-success').text('If the form has not opened in a new tab, please check your browser settings. You should receive a confirmation email shortly.');
+      }, 1200);
+    });
+  }
 })
